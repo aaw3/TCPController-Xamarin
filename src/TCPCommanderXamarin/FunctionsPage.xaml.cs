202bf7d87eeb21153c7b7e08ff438c866beb8c4a
Allen Wolf <63997543+aaw3@users.noreply.github.com>
2024-01-10 21:37:10 -0600

    Add project

---
+++ src/TCPCommanderXamarin/FunctionsPage.xaml.cs
@@ -0,0 +1,253 @@
+﻿using Android;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TCPCommanderXamarin.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Xaml;
+
+namespace TCPCommanderXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FunctionsPage : ContentPage
+    {
+        public static string storedprivIP;
+        public bool pressedBackButton;
+
+        public FunctionsPage()
+        {
+            InitializeComponent();
+        }
+
+        byte[] buffer = new byte[1];
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            NavigationPage.SetHasBackButton(this, false);
+
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                if (Connection.Instance.client != null)
+                {
+                    if (MainPage.clientIsOpen)
+                    {
+                        if (Connection.Instance.client.Client.Poll(0, SelectMode.SelectRead))
+                        {
+                            if (Connection.Instance.client.Client.Receive(buffer, SocketFlags.Peek) == 0)
+                            {
+                                ClientDisconnected();
+                            }
+                        }
+                    }
+                }
+
+                return true;
+            });
+
+            Device.StartTimer(TimeSpan.FromMilliseconds(250), () =>
+            {
+                if (pressedBackButton)
+                {
+                    BackButtonPressed();
+                }
+                return true;
+            });
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            pressedBackButton = true;
+            return true;
+        }
+
+        public async void BackButtonPressed()
+        {
+            pressedBackButton = false;
+            bool response = await DisplayAlert("Are You Sure?", "Do you wan't to disconnect and go to the login page?", "YES", "NO");
+            if (response)
+            {
+                Connection.Instance.client.GetStream().Close();
+                Connection.Instance.client.Close();
+                MainPage.clientIsOpen = false;
+                await Navigation.PushAsync(new MainPage());
+            }
+        }
+
+        public async void ClientDisconnected()
+        {
+            Connection.Instance.client.GetStream().Close();
+            Connection.Instance.client.Close();
+            MainPage.clientIsOpen = false;
+            MainPage.lostConnection = true;
+            await Navigation.PushAsync(new MainPage());
+        }
+
+        private void Test_Clicked(object sender, EventArgs e)
+        {
+            writeMessage("{TEST}");
+
+            byte[] bytes = getData(Connection.Instance.client);
+
+            string returnMessage = Encoding.ASCII.GetString(bytes);
+
+            if (returnMessage == "{TEST_RESPOND}")
+            {
+                DisplayAlert("Test Successful", "The server received the message and responded", "OK");
+            }
+        }
+
+        private async void Shutdown_Clicked(object sender, EventArgs e)
+        {
+            bool response = await DisplayAlert("Shutdown System", "Are you sure you want to force shutdown your computer?", "YES", "NO");
+            
+            if (response)
+            {
+                writeMessage("{SHUTDOWN}");
+
+                byte[] bytes = getData(Connection.Instance.client);
+
+                string returnMessage = Encoding.ASCII.GetString(bytes);
+
+                if (returnMessage == "{BEGAN_SHUTDOWN}")
+                {
+                    await DisplayAlert("Shutdown Successful", "The server has received the message and has begun shutdown", "OK");
+                }
+            }
+        }
+
+        private void MonitorOn_Clicked(object sender, EventArgs e)
+        {
+            writeMessage("{MONITOR_ON}");
+
+            byte[] bytes = getData(Connection.Instance.client);
+
+            string returnMessage = Encoding.ASCII.GetString(bytes);
+
+            if (returnMessage == "{MONITOR_TURNED_ON}")
+            {
+                DisplayAlert("Display Settings", "The server has received the message and has turned the monitor on", "OK");
+            }
+        }
+
+        private void MonitorOff_Clicked(object sender, EventArgs e)
+        {
+            writeMessage("{MONITOR_OFF}");
+
+            byte[] bytes = getData(Connection.Instance.client);
+
+            string returnMessage = Encoding.ASCII.GetString(bytes);
+
+            if (returnMessage == "{MONITOR_TURNED_OFF}")
+            {
+                DisplayAlert("Display Settings", "The server has received the message and has turned the monitor off", "OK");
+            }
+        }
+
+        private void TakeScreenshot_Clicked(object sender, EventArgs e)
+        {
+            writeMessage("{TAKE_SCREENSHOT}");
+
+            var data = getData(Connection.Instance.client);
+            imageView.Source = ImageSource.FromStream(() => new MemoryStream(data));
+            
+        }
+
+        Ping p = new Ping();
+        PingReply reply;
+        private void Ping_Clicked(object sender, EventArgs e)
+        {
+            int i = 0;
+
+            int pingAmt = 8;
+            int testPing = 1;
+
+            string netInfo = "";
+            do
+            {
+                reply = p.Send(storedprivIP, 1000);
+
+                if (reply != null)
+                {
+                    i++;
+
+                    if (i > testPing)
+                    {
+                        if (i <= pingAmt + 1)
+                        {
+                            netInfo += "Status : " + reply.Status.ToString() + "   Time : " + reply.RoundtripTime + "\n";
+                        }
+                        else
+                        {
+                            netInfo += "Status : " + reply.Status + "   Time : " + reply.RoundtripTime;
+                        }
+                    }
+                }
+                
+
+            }
+            while (i <= pingAmt + testPing
+            );
+
+            DisplayAlert("Ping: " + storedprivIP, netInfo, "OK");
+        }
+
+        public void writeMessage(string input)
+        {
+            TcpClient client = Connection.Instance.client;
+            NetworkStream ns = client.GetStream();
+            byte[] message = Encoding.ASCII.GetBytes(input);
+            ns.Write(message, 0, message.Length);
+        }
+
+        public byte[] getData(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] fileSizeBytes = new byte[4];
+            int bytes = stream.Read(fileSizeBytes, 0, fileSizeBytes.Length);
+            int dataLength = BitConverter.ToInt32(fileSizeBytes, 0);
+
+            int bytesLeft = dataLength;
+            byte[] data = new byte[dataLength];
+
+            int buffersize = 1024;
+            int bytesRead = 0;
+
+            while (bytesLeft > 0)
+            {
+                int curDataSize = Math.Min(buffersize, bytesLeft);
+                if (client.Available < curDataSize)
+                {
+                    curDataSize = client.Available;
+                }
+
+                bytes = stream.Read(data, bytesRead, curDataSize);
+                bytesRead += curDataSize;
+                bytesLeft -= curDataSize;
+            }
+
+            return data;
+        }
+
+        public static string getBetween(string strSource, string strStart, string strEnd)
+        {
+            int Start, End;
+            if (strSource.Contains(strStart) && strSource.Contains(strEnd))
+            {
+                Start = strSource.IndexOf(strStart, 0) + strStart.Length;
+                End = strSource.IndexOf(strEnd, Start);
+                return strSource.Substring(Start, End - Start);
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ src/TCPCommanderXamarin/MainPage.xaml.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using TCPCommanderXamarin.Droid;
+using System.Text.RegularExpressions;
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
+using Android.Widget;
+using System.IO;
+using Android.Runtime;
+using System.Xml.Serialization;
+using System.Net;
+//using DroidOS = Android.OS;
+//using Android.OS;
+//using Android.App;
+
+namespace TCPCommanderXamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        string preferencesFile = "/prefs.xml";
+        bool prefsExist;
+
+        StoredData readStoredData;
+        public static bool clientIsOpen;
+        public static bool lostConnection;
+        public bool readFile;
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            if (!readFile)
+            {
+                if (Directory.GetFiles(folderPath).Length > 0)
+                {
+                    if (File.Exists(folderPath + preferencesFile))
+                    {
+                        prefsExist = true;
+
+                        XmlSerializer Serializer = new XmlSerializer(typeof(StoredData));
+                        FileStream fs = new FileStream(folderPath + preferencesFile, FileMode.Open);
+
+                        readStoredData = (StoredData)Serializer.Deserialize(fs);
+                        ipAddress.Text = readStoredData.PrivateIPV4;
+                        Port.Text = readStoredData.Port;
+                        fs.Close();
+                    }
+                }
+            }
+
+            if (lostConnection)
+            {
+                ConnectionLost();
+            }
+
+            NavigationPage.SetHasBackButton(this, false);
+        }
+
+        public async void ConnectionLost()
+        {
+            bool response = await DisplayAlert("Client Disconnected", "The client has lost connection to the server.\nWould you like to reconnect?", "YES", "NO");
+
+            if (response)
+            {
+                client.GetStream().Close();
+                client.Close();
+                Connect_Clicked(new object(), new EventArgs());
+            }
+        }
+
+        public void lockButton(bool locked)
+        {
+
+            Device.BeginInvokeOnMainThread(() => { connectButton.IsEnabled = !locked; });
+        }
+
+        int tryConnectTime = 10000;
+        public async Task<TcpClient> tryConnect()
+        {
+            if (client == null)
+            {
+                client = new TcpClient();
+            }
+
+            var connectionTask = client.ConnectAsync(ipAddress.Text, Convert.ToInt32(Port.Text)).ContinueWith(task =>
+            {
+                return task.IsFaulted ? null : client;
+            }, TaskContinuationOptions.ExecuteSynchronously);
+            var timeoutTask = Task.Delay(tryConnectTime).ContinueWith<TcpClient>(task => null, TaskContinuationOptions.ExecuteSynchronously);
+            var resultTask = Task.WhenAny(connectionTask, timeoutTask).Unwrap();
+            resultTask.Wait();
+            var resultTcpClient = await resultTask;
+
+            return resultTcpClient;
+        }
+
+        public void MasterConnection()
+        {
+            lockButton(true);
+            client = tryConnect().Result;
+            lockButton(false);
+        }
+
+        TcpClient client = new TcpClient();
+        private async void Connect_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                MasterConnection();
+
+                if (client != null)
+                {
+                    clientIsOpen = true;
+                    Connection.Instance.client = client;
+                    NavigationPage.SetHasBackButton(this, false);
+                    await Navigation.PushAsync(new FunctionsPage());
+
+                    await DisplayAlert("Connected", "Connected to server successfully!", "Ok");
+                    FunctionsPage.storedprivIP = ipAddress.Text;
+
+                    if (!prefsExist || ipAddress.Text != readStoredData.PrivateIPV4 || Port.Text != readStoredData.Port)
+                    {
+                        XmlSerializer Serializer = new XmlSerializer(typeof(StoredData));
+                        StoredData SD = new StoredData();
+                        SD.PrivateIPV4 = ipAddress.Text;
+                        SD.Port = Port.Text;
+
+                        TextWriter Writer = new StreamWriter(folderPath + preferencesFile);
+                        Serializer.Serialize(Writer, SD);
+                        Writer.Close();
+                    }
+                }
+                else
+                {
+                    await DisplayAlert("Connection Unsuccessful", "Could not connect to the server!", "Ok");
+                    clientIsOpen = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", "" + ex.ToString(), "Ok");
+                clientIsOpen = false;
+            }
+        }
+
+        bool ipAddressIgnoreChange;
+        private void IPAddress_OnTextChanged(object sender, EventArgs e)
+        {
+            if (ipAddressIgnoreChange)
+            {
+                ipAddressIgnoreChange = false;
+                return;
+            }
+
+
+            if (Regex.IsMatch(ipAddress.Text, @"[^0-9.]"))
+            {
+                ipAddress.Text = Regex.Replace(ipAddress.Text, @"[^0-9.]", string.Empty);
+                ipAddressIgnoreChange = true;
+            }
+
+            if (ipAddress.Text.Contains(".."))
+            {
+                ipAddress.Text = ipAddress.Text.Replace("..", ".");
+                ipAddressIgnoreChange = true;
+            }
+        }
+
+        bool portIgnoreChange;
+        private void Port_OnTextChanged(object sender, EventArgs e)
+        {
+            if (portIgnoreChange)
+            {
+                portIgnoreChange = false;
+                return;
+            }
+
+
+            if (Regex.IsMatch(Port.Text, @"[^0-9]"))
+            {
+                Port.Text = Regex.Replace(Port.Text, @"[^0-9]", string.Empty);
+                portIgnoreChange = true;
+            }
+        }
+
+
+    }
+
+    public class StoredData
+    {
+        public string PrivateIPV4;
+        public string Port;
+    }
+
+    public static class StringFunctions
+    {
+        public static string RemoveLetters(this string str)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((str[i] >= 'a' && str[i] <= 'A') || (str[i] >= 'z' && str[i] <= 'Z'))
+                {
+                    sb.Append(str[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static string RemoveSpecialChars(this string str)
+        {
+            string[] chars = new string[] { ",", ".", "/", "!", "@", "#", "$", "%", "^", "&", "*", "'", "\"", ";", "_", "(", ")", ":", "|", "[", "]" };
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (str.Contains(chars[i]))
+                {
+                    str = str.Replace(chars[i], "");
+                }
+            }
+            return str;
+        }
+    }
+}
